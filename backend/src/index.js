fc557c18b698243ddb37892f3180653673aec527
Lara Freitas <alaraoliveira09@gmail.com>
2025-09-30 18:01:19 -0300

    Projeto inicial com frontend e backend

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+
+import { persons } from "./persons.js"
+
+const app = express()
+const port = 3333
+
+
+app.use(cors())
+
+const person = 
+
+app.get("/", (request, response) => {
+    response.json(persons)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}!`)
+})
---
+++ backend/src/persons.js
@@ -0,0 +1,22 @@
+const persons = [
+    
+    {
+    name: "Vitor",
+    age: 21,
+    city: "São Paulo"
+}, 
+
+{
+    name: "Laura",
+    age: 12,
+    city: "Belo Horizonte"
+},
+
+{
+    name: "Leonardo",
+    age: 17,
+    city: "São Bernardo do Campo"
+}
+]
+
+export { persons}
